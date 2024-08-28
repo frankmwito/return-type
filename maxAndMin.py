9bd6b184d68f7734ba3a481f17951cbdbbffaa1a
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-28 21:19:39 +0300

    maximum and minimum values from list

--- maxAndMin.py
+++ maxAndMin.py
@@ -1,2 +1,15 @@
 # return a list of maximum and minimum numbers in a list
 
+def maxand_min():
+    numbers = list()
+    num = int(input("Enter the size of your list:"))
+    
+    for i in range(num):
+        number = int(input(f"Enter a number {i+1}:"))
+        numbers.append(number)
+    
+    max_num = max(numbers)
+    print(max_num)
+    min_num = min(numbers)
+    return min_num
+print(maxand_min())
