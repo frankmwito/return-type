ff340363b9a5a733883b15d49b1fae078984e1d7
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-28 20:50:49 +0300

    check for prime numbers

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,20 @@
-# return a list of only prime numbers
+def prime_numbers():
+    prime_list = []
+    num = int(input("Enter size of the list:"))
+    
+    for i in range(num):
+        number = int(input(f"Enter a number for {i+1}:"))
+        if number > 1:  # Prime numbers are greater than 1
+            is_prime = True
+            for j in range(2, int(number ** 0.5) + 1):  # Check divisors up to the square root of the number
+                if number % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                prime_list.append(number)
+    
+    return prime_list
+
+# Example Usage
+prime_list = prime_numbers()
+print(f"The list of prime numbers is: {prime_list}")
