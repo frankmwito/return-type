0f60ea653f3bdcb1bd13173077fb71ebb57a3011
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-29 11:04:15 +0300

    GCD of two numbers

--- GCD.py
+++ GCD.py
@@ -1,3 +1,11 @@
 # find the greatest common divisor 
 # two integers
-    
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+a = int(input("Enter any number: "))
+b = int(input("Enter any number: "))
+print(gcd(a, b))
